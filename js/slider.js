2e207d7271005b07c18ca3bb3d90f0493e7c9e86
Maksym Kotyk <mxm.kotyk@gmail.com>
2022-11-19 21:44:55 +0100

    Added images, filled sliders

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".mySwiper", {
+            effect: "cards",
+            grabCursor: true,
+            // loop: true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
